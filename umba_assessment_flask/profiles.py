8b4d61d80cc9608e6331a5d113fa673a3bd2ab58
Hilal Jaffan <hj@jaffan.io>
2021-02-17 08:14:07 -0800

    Adding pagination

--- umba_assessment_flask/profiles.py
+++ umba_assessment_flask/profiles.py
@@ -13,18 +13,26 @@ bp = Blueprint('profiles', __name__, url_prefix='/profiles')
 @bp.route('/', methods=['GET'])
 def home():
     username = request.args.get('user')
-    offset = 0
+    page = 0
     per_page = 100
-    if request.args.get('offset'):
-        offset = int(request.args.get('offset'))
+    if request.args.get('page'):
+        page = int(request.args.get('page'))
+        offset = page * 100
 
     if username is not None:
         users = db.get_single_user(username)
+        page_count = (len(users) / 100)
     else:
         total, users = db.get_all_profiles(offset=offset, per_page=per_page)
-    final_users = []
+        page_count = int(total / 100)
+    user_json = []
     for user in users:
-        us = {"id": user[0], "username": str(user[1]).strip(), "profile_url": user[2], "type": str(user[3]).strip(),
-              "profile": user[4]}
-        final_users.append(us)
+        us = {"id": user[0], "username": str(user[1]).strip(), "avatar": user[2], "type": str(user[3]).strip(),
+              "profile": str(user[4]).strip()}
+        user_json.append(us)
+
+    final_users = dict()
+    final_users['page'] = page
+    final_users['total_pages'] = page_count
+    final_users['profiles'] = user_json
     return jsonify(final_users)
